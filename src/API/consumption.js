839c4c702ce4c52f14ede288ba4e504da45ea33a
sujilnt <sujilnt@gmail.com>
2019-02-05 12:29:58 +0000

    adding packages.json

--- src/API/consumption.js
+++ src/API/consumption.js
@@ -1,6 +1,12 @@
 const {fetchRequest}=require("./fetchRequest");
 const consumption =async (fromDate,toDate,deviceID,paramkey,params)=>{
   const _url= `http://api.dexcell.com/v3/readings?from=${fromDate}&to=${toDate}&device_id=${deviceID}&parameter_key=${paramkey}&resolution=${params}&operation=DELTA`;
-  return await fetchRequest(_url).then((data)=>data);
+  return await fetchRequest(_url).then((data)=>{
+    //console.log("data",data,fromDate,toDate,deviceID,paramkey,params);
+    return data;
+  }).catch((error)=>{
+    console.log("error",error,fromDate,toDate,deviceID,paramkey,params);
+    return error;
+  });
 };
 module.exports.consumption=consumption;
